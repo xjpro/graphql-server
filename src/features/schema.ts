31b8f74add5f46be9d8cabd341d533919be6cfc9
Joshua Prodahl <jprodahl@gmail.com>
2022-11-08 15:13:34 -0600

    make dist start work

--- src/features/schema.ts
+++ src/features/schema.ts
@@ -1,15 +1,10 @@
 import path from "path";
 import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
 import { loadFilesSync } from "@graphql-tools/load-files";
-import {makeExecutableSchema} from "@graphql-tools/schema";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
-const typeDefsArray = loadFilesSync(path.join(__dirname, "./**/*.gql"), {
-  extensions: ["gql"],
-});
-
-const resolversArray = loadFilesSync(
-  path.join(__dirname, "./**/*.resolver.ts")
-);
+const typeDefsArray = loadFilesSync(path.join(__dirname, "./**/*.gql"));
+const resolversArray = loadFilesSync(path.join(__dirname, "./**/*.resolver.*"));
 
 const schema = makeExecutableSchema({
   typeDefs: mergeTypeDefs(typeDefsArray),
